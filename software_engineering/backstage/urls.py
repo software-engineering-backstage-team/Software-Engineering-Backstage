0cfe2c2d3cbda4daedbdcc6bae3c531c4b507a25
cairui <18811610600@163.com>
2019-03-21 14:16:30 +0800

    change some urls settings

---
+++ software_engineering/backstage/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = "backstage"
+urlpatterns = [
+    path('', views.Welcome, name="Welcome"),
+    path('Login/', views.Login, name='Login'),
+    path('Log_out/', views.Log_out, name='Log_out'),
+    path('Register/', views.Register, name='Register'),
+    path('Homepage/', views.Homepage, name='Homepage'),
+]
